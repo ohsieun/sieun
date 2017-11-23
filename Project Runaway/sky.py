a3d3639825cb4ff4a20212641392bc3327a7ef06
ohsieun <32888232+ohsieun@users.noreply.github.com>
2017-11-24 00:10:31 +0900

    add resources

--- Project Runaway/sky.py
+++ Project Runaway/sky.py
@@ -4,7 +4,7 @@ from pico2d import *
 
 class Sky:
     def __init__(self):
-        self.image = load_image('sky.png')
+        self.image = load_image('sky_red_1.png')
 
     def draw(self):
         self.image.draw(480, 300)
